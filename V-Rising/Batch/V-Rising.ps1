d5ee5a37c718dd631fe83a2485436ed8cc706fae
GinoKarro <58960777+GinoKarro@users.noreply.github.com>
2023-05-05 12:05:08 -0400

    bsdfbsdb
    bsdfbsd

--- V-Rising/Batch/V-Rising.ps1
+++ V-Rising/Batch/V-Rising.ps1
@@ -1,6 +1,10 @@
 C:\#servers\steamcmd\steamcmd.exe +force_install_dir C:\#servers\v-rising\ +login anonymous +app_update 1829350 +quit
 timeout 5
-xcopy C:\#servers\v-rising\#Edit\bu\*.* C:\#servers\v-rising\VRisingServer_Data\StreamingAssets\Settings\ /Y
+cd C:\#Code\GameServers
+git pull origin main
+timeout 5
+xcopy C:\#Code\GameServers\V-Rising\CFG\*.* C:\#servers\v-rising\VRisingServer_Data\StreamingAssets\Settings\ /Y
+xcopy C:\#Code\GameServers\V-Rising\Batch\V-Rising.ps1 C:\#servers\ /Y
 timeout 5 
 cls
 echo "Starting V Rising Dedicated Server on port : 9876,9877 - PRESS CTRL-C to exit"
