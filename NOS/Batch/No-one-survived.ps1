bda1d0621111ba667447ac62c55877f5ec2ee39d
GinoKarro <58960777+GinoKarro@users.noreply.github.com>
2023-05-15 14:18:16 -0400

    Create No-one-survived.ps1

---
+++ NOS/Batch/No-one-survived.ps1
@@ -0,0 +1,25 @@
+#    #Set Dated Backups
+#    $date = (Get-Date).tostring("yyyyMMdd")
+#    $hour = (Get-Date).tostring("hhmmss")
+#    $save = 'NOS_' + $date + '_' + $hour + '_Backups'
+#    
+#    #Compress config
+#    $compress = @{
+#      Path = "C:\#servers\nos\WRSH\Saved\SaveGames"
+#      CompressionLevel = "Fastest"
+#      DestinationPath = "C:\#servers\Backup\No-One-Survived\SaveGames\$save.zip"
+#    }
+#    #Compress
+#    Compress-Archive @compress -force -verbose
+
+#Update Server VIA SteamCMD
+C:\#servers\steamcmd\steamcmd.exe +force_install_dir C:\#servers\NOS\ +login anonymous +app_update 2329680 validate +quit
+Timeout /T 10
+
+#   #Copy Config Files to Server Folder
+#   Copy-item -Path 'C:\#servers\Backup\No-One-Survived\Saved\Config\WindowsServer\*.ini' -destination 'C:\#servers\NOS\WRSH\Saved\Config\WindowsServer\' -Recurse -force -verbose
+#   Timeout /T 10
+
+#Start NOS Server
+C:\#servers\NOS\WRSHServer.exe -server -log
+Timeout /T 10
