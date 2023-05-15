5551993243861efc649a58e88c57683bb474c25e
GinoKarro <58960777+GinoKarro@users.noreply.github.com>
2023-05-15 13:55:52 -0400

    Update V-Rising.ps1
    bsdfb

--- V-Rising/Batch/V-Rising.ps1
+++ V-Rising/Batch/V-Rising.ps1
@@ -6,4 +6,4 @@ timeout 5
 Clear-Host
 Write-Output "Starting V Rising Dedicated Server on port : 9876,9877 - PRESS CTRL-C to exit"
 Set-Location C:\#servers\v-rising\
-C:\#servers\v-rising\VRisingServer.exe -persistentDataPath .\v-rising\save-data -serverName "-=QC PVE=- Virgin Tusk Rising" -saveName "TuskGG" -logFile ".\v-rising\logs\VRisingServer.log"
+C:\#servers\v-rising\VRisingServer.exe -persistentDataPath C:\#servers\#Save\v-rising\save-data -serverName "-=QC PVE=- Virgin Tusk Rising" -saveName "TuskGG" -logFile "C:\#servers\#Save\v-rising\logs\VRisingServer.log"
