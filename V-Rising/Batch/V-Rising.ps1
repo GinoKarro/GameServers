95f3e20fda6365b6760895405efa69cc73a2fac9
GinoKarro <58960777+GinoKarro@users.noreply.github.com>
2023-05-16 13:25:52 -0400

    Update V-Rising.ps1

--- V-Rising/Batch/V-Rising.ps1
+++ V-Rising/Batch/V-Rising.ps1
@@ -1,7 +1,9 @@
-xcopy C:\#Code\GameServers\V-Rising\CFG\* C:\#servers\v-rising\VRisingServer_Data\StreamingAssets\Settings\ /Y
-timeout 5 
+#xcopy C:\#Code\GameServers\V-Rising\CFG\* C:\#servers\v-rising\VRisingServer_Data\StreamingAssets\Settings\ /Y
+#timeout 5 
 C:\#servers\steamcmd\steamcmd.exe +force_install_dir C:\#servers\v-rising\ +login anonymous +app_update 1829350 +quit
 timeout 5 
+xcopy C:\#Code\GameServers\V-Rising\CFG\* C:\#servers\v-rising\VRisingServer_Data\StreamingAssets\Settings\ /Y
+timeout 5
 Clear-Host
 Write-Output "Starting V Rising Dedicated Server on port : 9876,9877 - PRESS CTRL-C to exit"
 C:\#servers\v-rising\VRisingServer.exe -persistentDataPath C:\#servers\#Save\v-rising\save-data -serverName "-=QC PVE=- Virgin Tusk Rising" -saveName "TuskGG" -logFile "C:\#servers\#Save\v-rising\logs\VRisingServer.log"
