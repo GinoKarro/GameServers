30f7bf27e81fd1ff30cad95e565c9c7bab3eaff9
GinoKarro <58960777+GinoKarro@users.noreply.github.com>
2023-05-15 20:18:21 -0400

    Update Frozen.ps1

--- Frozen/Batch/Frozen.ps1
+++ Frozen/Batch/Frozen.ps1
@@ -1,3 +1,4 @@
 C:\#servers\steamcmd\steamcmd.exe +force_install_dir C:\#servers\Frozen\ +login anonymous +app_update 1348640 validate +quit
 timeout 3
+xcopy C:\#Code\GameServers\Frozen\CFG\*.ini C:\#servers\frozen\FrozenFlame\Saved\Config\WindowsServer\ /Y
 C:\#servers\frozen\FrozenFlameServer.exe -log -LOCALLOGTIMES -MetaGameServerName="-=°Qc|Fr°=- Tusken Flame Server" -ip=184.162.16.65 -Port=27018 -queryPort=27019 -RconPort=25575 -RconPassword=ginette12
