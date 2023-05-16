7a5b72ca3106b6addc88bc3a67a4f311ebad7a44
GinoKarro <58960777+GinoKarro@users.noreply.github.com>
2023-05-15 20:06:53 -0400

    Create Frozen.ps1

---
+++ Frozen/Batch/Frozen.ps1
@@ -0,0 +1,3 @@
+C:\#servers\steamcmd\steamcmd.exe +force_install_dir C:\#servers\Frozen\ +login anonymous +app_update 1348640 validate +quit
+timeout 3
+C:\#servers\frozen\FrozenFlameServer.exe -log -LOCALLOGTIMES -MetaGameServerName="-=°Qc|Fr°=- Tusken Flame Server" -ip=184.162.16.65 -Port=27018 -queryPort=27019 -RconPort=25575 -RconPassword=ginette12
