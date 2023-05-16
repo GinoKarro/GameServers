c7b18e9af5c6e29799032bbd230e0195d8bb4885
GinoKarro <58960777+GinoKarro@users.noreply.github.com>
2023-05-15 20:52:53 -0400

    Update 7days.ps1

--- 7day/Batch/7days.ps1
+++ 7day/Batch/7days.ps1
@@ -2,6 +2,7 @@ C:\#servers\steamcmd\steamcmd.exe +force_install_dir C:\#servers\7days\ +login a
 Set-Location C:\#servers\7days
 timeout 3
 xcopy C:\#Code\GameServers\7day\CFG\serverconfig.xml C:\#servers\7days /Y
+xcopy C:\#Code\GameServers\7day\CFG\serveradmin.xml C:\#servers\#Save\7Days\Save-Data /Y
 timeout 3
 C:\#servers\7days\7DaysToDieServer.exe -quit -batchmode -nographics -SaveGameFolder=C:\#servers\#Save\7Days\Save-Data -configfile=serverconfig.xml -dedicated
 #SaveGameFolder
