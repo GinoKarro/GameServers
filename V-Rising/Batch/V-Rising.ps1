4ff1174c3019cb9a28e22f4e85352784776ff56c
GinoKarro <58960777+GinoKarro@users.noreply.github.com>
2023-05-16 13:29:18 -0400

    Update V-Rising.ps1

--- V-Rising/Batch/V-Rising.ps1
+++ V-Rising/Batch/V-Rising.ps1
@@ -1,5 +1,3 @@
-#xcopy C:\#Code\GameServers\V-Rising\CFG\* C:\#servers\v-rising\VRisingServer_Data\StreamingAssets\Settings\ /Y
-#timeout 5 
 C:\#servers\steamcmd\steamcmd.exe +force_install_dir C:\#servers\v-rising\ +login anonymous +app_update 1829350 +quit
 timeout 5 
 xcopy C:\#Code\GameServers\V-Rising\CFG\* C:\#servers\v-rising\VRisingServer_Data\StreamingAssets\Settings\ /Y
