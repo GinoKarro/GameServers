e1d26f117b9d8e86c976dff902b9bc2de890b803
GinoKarro <58960777+GinoKarro@users.noreply.github.com>
2023-06-21 09:55:17 -0400

    Update DoD.ps1

--- DoD/Batch/DoD.ps1
+++ DoD/Batch/DoD.ps1
@@ -1,9 +1,10 @@
+
 C:\#servers\steamcmd\steamcmd.exe +force_install_dir C:\#servers\DoD\ +login anonymous +app_update 232290 +quit
 Set-Location C:\#servers\DoD
 timeout 3
-#xcopy C:\#Code\GameServers\7day\CFG\serverconfig.xml C:\#servers\7days /Y
-#xcopy C:\#Code\GameServers\7day\CFG\serveradmin.xml C:\#servers\#Save\7Days\Save-Data /Y
+xcopy C:\#Code\GameServers\DoD\Config\server.cfg C:\#servers\DoD\dod\server.cfg /Y
 #timeout 3
 #C:\#servers\7days\7DaysToDieServer.exe -quit -batchmode -nographics -SaveGameFolder=C:\#servers\#Save\7Days\Save-Data -configfile=serverconfig.xml -dedicated
+srcds.exe -console -game dod +port 27015 +map dod_anzio -autoupdate +maxplayers 32
 #SaveGameFolder
 
