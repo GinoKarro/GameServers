b2ef76f273e1d4fedd406fd97ba4640923755cce
GinoKarro <58960777+GinoKarro@users.noreply.github.com>
2023-06-29 10:22:32 -0400

    Update 7days.ps1

--- 7day/Batch/7days.ps1
+++ 7day/Batch/7days.ps1
@@ -4,6 +4,6 @@ timeout 3
 xcopy C:\#Code\GameServers\7day\CFG\serverconfig.xml C:\#servers\7days /Y
 xcopy C:\#Code\GameServers\7day\CFG\serveradmin.xml C:\#servers\#Save\7Days\Save-Data /Y
 timeout 3
-C:\#servers\7days\7DaysToDieServer.exe -quit -batchmode -nographics -SaveGameFolder=C:\#servers\#Save\7Days\Save-Data -configfile=serverconfig.xml -dedicated
+C:\#servers\7days\7DaysToDieServer.exe -quit -batchmode -nographics -SaveGameFolder=C:\#servers\#Save\7Days\Save-Data -configfile=serverconfig.xml -dedicated -logs
 #SaveGameFolder
 
